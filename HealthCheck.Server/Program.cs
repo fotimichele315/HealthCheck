890104ae5f044ba8f81153951c5548c8eb381d37
fotimichele315 <fotimichele315@gmail.com>
2025-12-23 20:54:05 +0100

    UseSwaggerUi

--- HealthCheck.Server/Program.cs
+++ HealthCheck.Server/Program.cs
@@ -1,28 +1,30 @@
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApiDocument(); // NSwag
 
 var app = builder.Build();
 
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+// Solo in Development
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();        // JSON OpenAPI
+    app.UseSwaggerUi();     // UI interattiva
+}
+
+// Mappa controller API
+app.MapControllers();
+
+// Angular SPA
 app.UseDefaultFiles();
 app.MapStaticAssets();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
+// **Il fallback deve essere l'ultima cosa**
 app.MapFallbackToFile("/index.html");
 
 app.Run();
