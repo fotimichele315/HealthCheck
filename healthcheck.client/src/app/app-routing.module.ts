bffb138f33514abed2b14a77990ff8a9ed5eda63
fotimichele315 <fotimichele315@gmail.com>
2026-01-10 08:08:31 +0100

    Fix routing and navigation component

--- healthcheck.client/src/app/app-routing.module.ts
+++ healthcheck.client/src/app/app-routing.module.ts
@@ -5,8 +5,9 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { HealthCheckComponent } from './health-check/health-check.component';
 const routes: Routes = [
   {path: '', component:HomeComponent, pathMatch: 'full'},
-  { path: 'fetch-data', component: FetchDataComponent } ,
-  { path: 'health-check', component: HealthCheckComponent } 
+  { path: 'health-check', component: HealthCheckComponent },
+  { path: 'fetch-data', component: FetchDataComponent },
+  
 ];
 
 @NgModule({
